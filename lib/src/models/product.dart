3a95ef562aff22a1a89b24a7ed084fc9965d4cf3
devrnt <devrient.jonas@gmail.com>
2019-11-11 20:13:53 +0100

    chore: add product model

---
+++ lib/src/models/product.dart
@@ -0,0 +1,39 @@
+class Product {
+  String title;
+  String description;
+  String imageUrl;
+  num price;
+
+  Product({this.title, this.description, this.imageUrl, this.price});
+}
+
+final initialProducts = [
+  Product(
+    title: 'Schoen',
+    description: 'Mijn schoen',
+    imageUrl:
+        'https://www.kenshoefashion.be/media//tag_stream/42459017_327060564716364_5708551427537675932_n.jpg',
+    price: 39.99,
+  ),
+  Product(
+    title: 'Schoen',
+    description: 'Mijn schoen',
+    imageUrl:
+        'https://www.kenshoefashion.be/media//tag_stream/42459017_327060564716364_5708551427537675932_n.jpg',
+    price: 39.99,
+  ),
+  Product(
+    title: 'Schoen',
+    description: 'Mijn schoen',
+    imageUrl:
+        'https://www.kenshoefashion.be/media//tag_stream/42459017_327060564716364_5708551427537675932_n.jpg',
+    price: 39.99,
+  ),
+  Product(
+    title: 'Schoen',
+    description: 'Mijn schoen',
+    imageUrl:
+        'https://www.kenshoefashion.be/media//tag_stream/42459017_327060564716364_5708551427537675932_n.jpg',
+    price: 39.99,
+  ),
+];
