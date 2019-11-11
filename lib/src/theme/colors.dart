c18ec3a080e645b2b0a9893468894bbb9e1e0d52
devrnt <devrient.jonas@gmail.com>
2019-11-11 19:57:16 +0100

    chore: add theme colors

---
+++ lib/src/theme/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+const kPrimary = Color(0xFFFFFFFF);
+
+const kPrimaryDark = Color(0xFF020202);
+const kPrimaryLight = Color(0xFF9C99AE);
+
+const kAccent = Color(0xFF3A27CE);
+const kAccentLight = Color(0xFFB7ACD2);
+const kAccentDark = Color(0xFF0D038C);
+
+const kScaffoldBackground = Color(0xFFF5F4FA);
