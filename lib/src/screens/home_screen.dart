4258e0387af8b30627bb85221039b320bdd1dcb6
devrnt <devrient.jonas@gmail.com>
2019-11-30 15:46:56 +0100

    chore: add provider to product list

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:store_it/src/models/product.dart';
+import 'package:provider/provider.dart';
+import 'package:store_it/src/models/store.dart';
 import 'package:store_it/src/widgets/product/product_list.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ProductList(products: initialProducts);
+    return Consumer<Store>(
+      builder: (context, model, child) {
+        return ProductList(products: model.products);
+      },
+    );
   }
 }
