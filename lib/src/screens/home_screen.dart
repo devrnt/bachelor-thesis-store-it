aadf4819105d69967c6032818b64ca51f627d307
devrnt <devrient.jonas@gmail.com>
2019-12-01 21:17:27 +0100

    chore: add mobx to product list

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:store_it/src/models/product.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+import 'package:store_it/src/mobx/store/product_store.dart';
 import 'package:store_it/src/widgets/product/product_list.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ProductList(products: initialProducts);
+    final productStore = Provider.of<ProductStore>(context);
+
+    return Observer(
+      builder: (context) {
+        return ProductList(products: productStore.products);
+      },
+    );
   }
 }
