e994ad92e6965894a64bb8491b46f57c25c330b0
devrnt <devrient.jonas@gmail.com>
2019-12-01 09:39:43 +0100

    chore: add redux actions for products state

---
+++ lib/src/redux/actions/actions.dart
@@ -0,0 +1,20 @@
+
+import 'package:store_it/src/redux/models/product.dart';
+
+class SetProductsAction {
+  final List<Product> products;
+
+  SetProductsAction(this.products);
+}
+
+class AddProductAction {
+  final Product product;
+
+  AddProductAction(this.product);
+}
+
+class RemoveProductAction {
+  final Product product;
+
+  RemoveProductAction(this.product);
+}
