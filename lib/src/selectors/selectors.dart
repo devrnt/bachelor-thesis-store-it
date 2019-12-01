1b8d72492721ae95cd362067ddc8a79bd34deb22
devrnt <devrient.jonas@gmail.com>
2019-12-01 09:56:33 +0100

    chore: add products selector

---
+++ lib/src/selectors/selectors.dart
@@ -0,0 +1,6 @@
+
+import 'package:store_it/src/redux/models/app_state.dart';
+import 'package:store_it/src/redux/models/product.dart';
+
+List<Product> productsSelector(AppState state) => state.products;
+
