6ec7f2f4e49149974a360c3732022a28c176a0be
devrnt <devrient.jonas@gmail.com>
2019-12-01 21:16:55 +0100

    chore: add mobx product store model

---
+++ lib/src/mobx/store/product_store.dart
@@ -0,0 +1,21 @@
+import 'package:mobx/mobx.dart';
+import 'package:store_it/src/models/product.dart';
+
+part 'product_store.g.dart';
+
+class ProductStore = _ProductStore with _$ProductStore;
+
+abstract class _ProductStore with Store {
+  @observable 
+  List<Product> products = initialProducts;
+
+  @action 
+  void addProduct(Product product){
+    products.add(product);
+  }
+
+  @action 
+  void removeProduct(Product product){
+    products.remove(product);
+  }
+}
---
+++ lib/src/mobx/store/product_store.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$ProductStore on _ProductStore, Store {
+  final _$productsAtom = Atom(name: '_ProductStore.products');
+
+  @override
+  List<Product> get products {
+    _$productsAtom.context.enforceReadPolicy(_$productsAtom);
+    _$productsAtom.reportObserved();
+    return super.products;
+  }
+
+  @override
+  set products(List<Product> value) {
+    _$productsAtom.context.conditionallyRunInAction(() {
+      super.products = value;
+      _$productsAtom.reportChanged();
+    }, _$productsAtom, name: '${_$productsAtom.name}_set');
+  }
+
+  final _$_ProductStoreActionController =
+      ActionController(name: '_ProductStore');
+
+  @override
+  void addProduct(Product product) {
+    final _$actionInfo = _$_ProductStoreActionController.startAction();
+    try {
+      return super.addProduct(product);
+    } finally {
+      _$_ProductStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void removeProduct(Product product) {
+    final _$actionInfo = _$_ProductStoreActionController.startAction();
+    try {
+      return super.removeProduct(product);
+    } finally {
+      _$_ProductStoreActionController.endAction(_$actionInfo);
+    }
+  }
+}
